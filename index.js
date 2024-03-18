66975a2c05e91f8f9d55b5323143c44c311a0743
Janison Silva Mendes <99850345+JanisonMendes@users.noreply.github.com>
2024-03-17 23:33:41 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var elementosDuvida = document.querySelectorAll('.duvida')
+
+elementosDuvida.forEach(function (duvida) {
+    duvida.addEventListener('click', function (){
+        duvida.classList.toggle('ativa')
+    })
+
+})
+   
